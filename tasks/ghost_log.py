c280db7982214991e5b2c1564424827043617c0a
f0xy <alex.foxy.rain@gmail.com>
2020-08-27 02:02:44 +0000

    Added playing in game...

--- tasks/ghost_log.py
+++ tasks/ghost_log.py
@@ -80,10 +80,13 @@ async def file_tail(bot, config, sleep_time):
             print("Encountered unknown character in server log, skipping lines.")
         else:
             for line in lines:    # Not EOF
-
+                print(line)
                 # if not "Watched user" in line:
                 #     await log_channel.send(monospace(line))
                 # replay_file = line
+                if "In games:" in line:
+                    await bot.change_presence(line)
+            
                 if 'saving data to file' in line:
                     print("Replay file created")
 
