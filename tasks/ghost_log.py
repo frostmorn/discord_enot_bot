a2a90ace0959c701e840eb1681d1c30a474c1aaf
f0xy <alex.foxy.rain@gmail.com>
2020-08-27 02:22:11 +0000

    Update

--- tasks/ghost_log.py
+++ tasks/ghost_log.py
@@ -85,7 +85,7 @@ async def file_tail(bot, config, sleep_time):
                 # replay_file = line
                 
                 if "In games:" in line:
-                    await bot.change_presence(activity=discord.Game(name=line[line.find("]"):].strip()))
+                    await bot.change_presence(activity=discord.Game(name=line[line.find("]")+1:].strip()))
             
                 if 'saving data to file' in line:
                     print("Replay file created")
