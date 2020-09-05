00b78f2883b64c370565a17d4cf6abb2740c7cd3
f0xy <alex.foxy.rain@gmail.com>
2020-09-05 00:34:27 +0000

    apm_data to console

--- tasks/ghost_log.py
+++ tasks/ghost_log.py
@@ -89,7 +89,6 @@ async def file_tail(bot, config, sleep_time):
             
                 if 'saving data to file' in line:
                     print("Replay file created")
-
                     replay_file = line.split("[")[3].replace("]", "").replace("\n", "").replace("\r", "")
 
                     await bugs_and_replays_channel.send(file=discord.File(replay_file))
@@ -97,8 +96,9 @@ async def file_tail(bot, config, sleep_time):
 
                     embed = discord.Embed(title="Players APM:  "+replay_file, colour=discord.Colour(0x000000))
                     apm_data = get_apm_data(replay_file)
+                    print(apm_data)
                     for apm_element in apm_data:
-                        embed.add_field(name=apm_element["player"], value=apm_data["apm"])
+                        embed.add_field(name=apm_element["player"], value=str(apm_data["apm"]))
                         
                     await bugs_and_replays_channel.send(embed=embed)
                     # TGHISDASD ASD TODO: MAKE THAT WORK
