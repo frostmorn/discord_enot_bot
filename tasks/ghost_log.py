fdd3d18b853148c80d84e70f7e6b0693a26294ad
f0xy <alex.foxy.rain@gmail.com>
2020-10-10 20:07:52 +0000

    update

--- tasks/ghost_log.py
+++ tasks/ghost_log.py
@@ -87,7 +87,8 @@ async def file_tail(bot, config, sleep_time):
                                     if not "Online Players" in line:
                                         if not "[REPLAY]"  in line:
                                             if not "[PACKED]" in line:
-                                                await log_channel.send(monospace(line))
+                                                if "joined the game" in line:
+                                                    await log_channel.send(monospace(line))
                 # replay_file = line
 
                     if "Online Players" in line:
