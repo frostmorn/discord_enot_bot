c60435cb8520ab2703563f4b1a286fa9d69da9aa
f0xy <alex.foxy.rain@gmail.com>
2020-12-31 02:15:45 +0000

    Switch to log only mode

--- fiona.py
+++ fiona.py
@@ -22,31 +22,31 @@ with open("config.json") as config_file:
 bot = commands.Bot(config["command_triger"])
 
 
-bot.add_cog(map_commands.Maps(bot))
+# bot.add_cog(map_commands.Maps(bot))
 
-bot.add_cog(base_commands.Base(bot))
+# bot.add_cog(base_commands.Base(bot))
 
-bot.add_cog(bot_commands.Bot(bot))
-bot.add_cog(reactions_commands.Reactions(bot))
-bot 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        return
-    raise error
-@bot.event
-async def on_message(message):
-    if message.attachments:
-        for attachment in message.attachments:
-            if attachment.filename.endswith('.w3x') or attachment.filename.endswith('.w3m'):
-                await map_download(bot, config, attachment, message)
-    await bot.process_commands(message)
+# bot.add_cog(bot_commands.Bot(bot))
+# bot.add_cog(reactions_commands.Reactions(bot))
+# bot 
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, CommandNotFound):
+#         return
+#     raise error
+# @bot.event
+# async def on_message(message):
+#     if message.attachments:
+#         for attachment in message.attachments:
+#             if attachment.filename.endswith('.w3x') or attachment.filename.endswith('.w3m'):
+#                 await map_download(bot, config, attachment, message)
+#     await bot.process_commands(message)
 
 
 
 bot.loop.create_task(ghost_log.file_tail(bot, config, 1))
-bot.loop.create_task(del_bot_shit.del_bot_shit(bot, config, 1))
-bot.loop.create_task(role_update.role_update(bot, config, 1))
+# bot.loop.create_task(del_bot_shit.del_bot_shit(bot, config, 1))
+# bot.loop.create_task(role_update.role_update(bot, config, 1))
 
 # run bot
 bot.run(config["token"])
