2ae06477a8eb39af6bfeddbbb6338583f40aca20
Orphey <15167344+frostmorn@users.noreply.github.com>
2021-03-14 02:01:38 +0000

    improvements

--- commands/bug.py
+++ commands/bug.py
@@ -63,7 +63,7 @@ class Bug(Cog):
                             } 
                         )
         self.bugs_save()
-        embed = Embed(title="Bug ID "+str(self.bugs[-1]["no"]+ "[added]"))
+        embed = Embed(title="Bug ID "+str(self.bugs[-1]["no"]+ "[ added 🐛 ]"))
         embed.add_field(name="Name:", value=self.bugs[-1]["name"])
         embed.add_field(name="author", value=self.bugs[-1]["author"])
         await ctx.send(embed=embed)
@@ -82,7 +82,7 @@ class Bug(Cog):
             return       
         for bug in self.bugs:
             if no == bug["no"]:
-                embed = Embed(title="Bug ID "+str(self.bugs[-1]["no"]+ "[deleted]"))
+                embed = Embed(title="Bug ID "+str(self.bugs[-1]["no"]+ "[ deleted 🚮 ]"))
                 embed.add_field(name="Name:", value=self.bugs[-1]["name"])
                 embed.add_field(name="author", value=self.bugs[-1]["author"])
                 await ctx.send(embed=embed)  
